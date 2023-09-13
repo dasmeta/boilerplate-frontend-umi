55510d360ff3204b1d5afb9eef9c5bcdee845381
VaheDasMeta <vahe.zhamharyan@tutor-platform.com>
2023-09-13 12:10:28 +0400

    rm comments

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { Login as LoginComponent } from "@dasmeta/auth-interface";
-import { history, useModel } from "@umijs/max";
+import { useModel } from "@umijs/max";
 import { Row } from "antd";
 import React from "react";
 
@@ -11,14 +11,6 @@ import React from "react";
 const Login: React.FC = () => {
   const { initialState }: any = useModel("@@initialState");
   const { settings } = initialState;
-
-  function onLoginSuccess(jwt: string | undefined) {
-    if (!jwt) {
-      return;
-    }
-    localStorage.setItem("jwtToken", jwt || "");
-    history.push("/");
-  }
 
   return (
     <Row justify={"center"} align={"middle"} style={{ height: "100%" }}>
