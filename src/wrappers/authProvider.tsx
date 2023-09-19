fa32adb2f4e5dcb8fb7c56fbe970c1dc7dc8da41
VaheDasMeta <vahe.zhamharyan@tutor-platform.com>
2023-09-19 15:37:27 +0400

    theme fixing

--- src/wrappers/authProvider.tsx
+++ src/wrappers/authProvider.tsx
@@ -1,11 +1,17 @@
 import { AuthProvider, Provider } from "@dasmeta/auth-interface";
-import { history } from "@umijs/max";
+import { history, useModel } from "@umijs/max";
 import { Outlet } from "umi";
 
 export default (children: any) => {
+  const { initialState }: any = useModel("@@initialState");
+  const { settings } = initialState;
   return (
     <AuthProvider
       provider={Provider.STRAPI}
+      theme={{
+        colorPrimary: settings.colorPrimary,
+        borderRadius: settings.borderRadius,
+      }}
       config={{
         host: BACKEND_HOST_EXAMPLE,
         onLoginSuccess: ({ jwt }) => {
